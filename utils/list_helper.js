53ad16c2ed6410b972a9a747d8664b30422cd790
Niko Kemppi <niko.kemppi@morssoftware.com>
2023-02-10 23:34:57 +0200

    Step4 helper functions and unit tests step 2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,15 @@
 const dummy = (blogs) => {
     return 1
 }
+
+const totalLikes = (blogs) => {
+    let likes = 0
+    for (let i in blogs) {
+        likes += blogs[i].likes
+    }
+    return likes
+}
   
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
