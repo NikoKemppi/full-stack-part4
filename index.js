0587421fbed4c94cf102bf069e947ad586bfc854
Niko Kemppi <niko.kemppi@morssoftware.com>
2023-02-10 23:50:10 +0200

    Part4 reformat documents

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const config = require('./utils/config')
 const app = require('./app')
 
 app.listen(config.PORT, () => {
-    logger.info(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on port ${config.PORT}`)
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,15 +1,15 @@
 const dummy = (blogs) => {
-    return 1
+  return 1
 }
 
 const totalLikes = (blogs) => {
-    let likes = 0
-    for (let i in blogs) {
-        likes += blogs[i].likes
-    }
-    return likes
+  let likes = 0
+  for (let i in blogs) {
+    likes += blogs[i].likes
+  }
+  return likes
 }
-  
+
 module.exports = {
-    dummy, totalLikes
+  dummy, totalLikes
 }
