4ffda1b18c19a7e492cc30dbae9e0a3e376a4146
Niko Kemppi <niko.kemppi@morssoftware.com>
2023-02-16 20:47:43 +0200

    Part4 bloglist expansion step 8

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
 
 mongoose.set('strictQuery', false)
 
@@ -23,6 +24,8 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
