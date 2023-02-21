13eb301dd130192a10df575d23acae1637406ac9
Niko Kemppi <niko.kemppi@morssoftware.com>
2023-02-21 20:00:43 +0200

    Empty list test added

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -69,6 +69,11 @@ describe('total likes', () => {
       __v: 0
     }
   ]
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
   
   test('when list has only one blog, equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithOneBlog)
